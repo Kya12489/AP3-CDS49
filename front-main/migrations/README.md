20b0ee9370c0615c73f9740cb0b2b552b5aae39f
Lawrence Richard <lrichard@dombtsig.local>
2025-09-05 16:31:22 +0200

    test pull main

--- front-main/migrations/README.md
+++
@@ -1,1 +0,0 @@
-Vous pouvez placer ici vos fichiers SQL permettant d'initialiser ou de modifier la base de données.
--- mobile-main/android/app/src/main/kotlin/com/example/mobil_cds49/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.mobil_cds49
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
