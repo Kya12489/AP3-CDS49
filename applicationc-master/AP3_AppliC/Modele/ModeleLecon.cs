16c4e42a9113a36f442ce41066ec2c3972349c7b
Sam <slevalegant@dombtsig.local>
2025-11-13 23:27:42 +0100

    début planning

---
+++ applicationc-master/AP3_AppliC/Modele/ModeleLecon.cs
@@ -0,0 +1,65 @@
+﻿using AP3_AppliC.Entities;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AP3_AppliC.Modele
+{
+    public class ModeleLecon
+    {
+        public static List<Conduire> RecupererLeconsPeriode(DateTime debut, DateTime fin)
+        {
+            try
+            {
+                return Connexion.MonModel.Conduires
+                    .Where(l => l.Heuredebut >= debut && l.Heuredebut <= fin)
+                    .ToList();
+            }
+            catch
+            {
+                return new List<Conduire>();
+            }
+        }
+
+        public static List<object> RecupererDisponibilites(
+            DateTime dateHeure,
+            int? idEleve,
+            int? idMoniteur,
+            int? idVehicule)
+        {
+            var resultats = new List<object>();
+
+            // Logique pour afficher les disponibilités selon ce qui est déjà sélectionné
+            // À implémenter selon tes besoins
+
+            return resultats;
+        }
+
+        public static bool AjouterLecon(int idEleve, int idMoniteur, int idVehicule, DateTime dateHeure, string lieu)
+        {
+            try
+            {
+                Conduire lecon = new Conduire()
+                {
+                    Ideleve = idEleve,
+                    Idmoniteur = idMoniteur,
+                    Idvehicule = idVehicule,
+                    Heuredebut = dateHeure,
+                    Lieurdv = string.IsNullOrWhiteSpace(lieu) ? null : lieu,
+                };
+
+                Connexion.MonModel.Conduires.Add(lecon);
+                Connexion.MonModel.SaveChanges();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return false;
+            }
+        }
+    }
+}
--- applicationc-master/AP3_AppliC/view/FormListeLecon.Designer.cs
+++ applicationc-master/AP3_AppliC/view/FormListeLecon.Designer.cs
@@ -28,19 +28,43 @@
         /// </summary>
         private void InitializeComponent()
         {
+            pnlCalendrier = new Panel();
+            lblMoisAnnee = new Label();
             SuspendLayout();
+            // 
+            // pnlCalendrier
+            // 
+            pnlCalendrier.Location = new Point(37, 56);
+            pnlCalendrier.Name = "pnlCalendrier";
+            pnlCalendrier.Size = new Size(721, 352);
+            pnlCalendrier.TabIndex = 0;
+            // 
+            // lblMoisAnnee
+            // 
+            lblMoisAnnee.AutoSize = true;
+            lblMoisAnnee.Location = new Point(422, 22);
+            lblMoisAnnee.Name = "lblMoisAnnee";
+            lblMoisAnnee.Size = new Size(50, 20);
+            lblMoisAnnee.TabIndex = 1;
+            lblMoisAnnee.Text = "label1";
             // 
             // FormListeLecon
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(lblMoisAnnee);
+            Controls.Add(pnlCalendrier);
             FormBorderStyle = FormBorderStyle.None;
             Name = "FormListeLecon";
             Text = "FormListeLecon";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Panel pnlCalendrier;
+        private Label lblMoisAnnee;
     }
 }
