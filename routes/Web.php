7ca425dde74e13a52db73b87f76d36f961ade1bf
lrichard <lawric49@gmail.com>
2026-05-05 11:26:50 +0200

    change: nom du fichier apk

--- routes/Web.php
+++ routes/Web.php
@@ -44,7 +44,7 @@ class Web
         Route::Add('/download-app', function () {
             $file = __DIR__ . '/../public/downloads/cds-app-release.apk';
             header('Content-Type: application/vnd.android.package-archive');
-            header('Content-Disposition: attachment; filename="app.apk"');
+            header('Content-Disposition: attachment; filename="cds-app.apk"');
             header('Content-Length: ' . filesize($file));
             readfile($file);
             exit;
